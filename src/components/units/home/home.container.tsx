1c088bf99172481f275db024d31f56785ae6becd
takyoung618 <wisdom8751@gmail.com>
2022-10-05 21:02:56 +0900

    :sparkles: Add: New Arrival 8Items complete

--- src/components/units/home/home.container.tsx
+++ src/components/units/home/home.container.tsx
@@ -1,6 +1,6 @@
 import { useQuery } from "@apollo/client";
 import { useState } from "react";
-import { FETCH_USEDITEMS, FETCH_USEDITEMS_OF_THE_BEST } from "../main/Main.queries";
+import { FETCH_USEDITEMS} from "../main/Main.queries";
 import HomePresenter from "./home.presenter";
 
 export default function HomeContainer(){
